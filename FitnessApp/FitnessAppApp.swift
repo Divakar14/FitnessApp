c0638d7b17466f33e07db88c5603dcababe916a7
Divakar <trdiva2608@gmail.com>
2025-04-08 05:59:38 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Divakar T R on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
